eff7f9e33bea3b2ae082e19e40353185aa0494bb
Jake Klassen <jklassendev@gmail.com>
2015-12-12 12:20:23 -0500

    warning logger and pm2 config change

--- app/lib/log.js
+++ app/lib/log.js
@@ -9,3 +9,7 @@ exports.error = debug('app:error');
 const info = debug('app:info');
 info.log = console.log.bind(console);
 exports.info = info;
+
+const warning = debug('app:warning');
+warning.log = console.warn.bind(console);
+exports.warning = warning;
